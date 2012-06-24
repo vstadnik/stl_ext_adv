451074c687a2db3bb6878b4fdaa4eb7095e7aeb2
Vadim Stadnik <vadimstdk@gmail.com>
2012-06-24 21:02:51 +1000

    update code for array based B+ trees

--- test/test_containers.hpp
+++ test/test_containers.hpp
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////////
 //
-//          Copyright Vadim Stadnik 2011.
+//          Copyright Vadim Stadnik 2011-2012.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
@@ -20,6 +20,7 @@
 #include "bp_tree.hpp"
 #include "bp_tree_idx.hpp"
 #include "bp_tree_idx_acc.hpp"
+#include "bp_tree_array.hpp"
 #include "bpt_map.hpp"
 #include "bpt_sequence.hpp"
 #include "bpt_set.hpp"
@@ -27,6 +28,7 @@
 
 namespace test_bpt
 {
+
     //  abbreviate names of types to simplify support
     //  for 80 characters requirement
     typedef size_t                                  _T   ;
@@ -36,6 +38,7 @@ namespace test_bpt
     typedef std::allocator<_T2>                     _AT2 ;
 
     #define _CBT container_bptree
+
 
 
     //  test STL variants of containers using class bp_tree ,
@@ -57,16 +60,16 @@ namespace test_bpt
     //  test STL variants of containers using class bp_tree_idx ;
     void adapters_idx ( const size_t  sz_test , const size_t n_dupl )
     {
-        //  generate classes of STL containers using 
-        //  explicit specification of template arguments 
+        //  generate classes of STL containers using
+        //  explicit specification of template arguments
     //  _CBT::sequence<_T, _AT,_CBT::bp_tree_idx>               t_seqce ;
     //  _CBT::set     <_T, _Ls, _AT, _CBT::bp_tree_idx>         t_set   ;
     //  _CBT::multiset<_T, _Ls, _AT, _CBT::bp_tree_idx>         t_mset  ;
     //  _CBT::map     <_T, _T , _Ls, _AT2, _CBT::bp_tree_idx>   t_map   ;
     //  _CBT::multimap<_T, _T , _Ls, _AT2, _CBT::bp_tree_idx>   t_mmap  ;
 
-        //  generate classes of STL containers using 
-        //  default arguments for template parameters 
+        //  generate classes of STL containers using
+        //  default arguments for template parameters
         _CBT::sequence<_T>      t_seqce ;
         _CBT::set     <_T>      t_set   ;
         _CBT::multiset<_T>      t_mset  ;
@@ -97,9 +100,33 @@ namespace test_bpt
         test_multi_map( t_mmap , sz_test , n_dupl ) ;
     }
 
+
+    //  test STL variants of containers using class bp_tree_array ;
+    void adapters_array ( const size_t  sz_test , const size_t n_dupl )
+    {
+        _CBT::sequence<_T, _AT,_CBT::bp_tree_array>               t_seqce ;
+        _CBT::set     <_T, _Ls, _AT, _CBT::bp_tree_array>         t_set   ;
+        _CBT::multiset<_T, _Ls, _AT, _CBT::bp_tree_array>         t_mset  ;
+        _CBT::map     <_T, _T , _Ls, _AT2, _CBT::bp_tree_array>   t_map   ;
+        _CBT::multimap<_T, _T , _Ls, _AT2, _CBT::bp_tree_array>   t_mmap  ;
+
+        test_sequence ( t_seqce, sz_test , n_dupl ) ;
+        test_set      ( t_set  , sz_test , n_dupl ) ;
+        test_multi_set( t_mset , sz_test , n_dupl ) ;
+        test_map      ( t_map  , sz_test , n_dupl ) ;
+        test_multi_map( t_mmap , sz_test , n_dupl ) ;
+    }
+
 }
 
 
 #endif  //  _TEST_CONTAINERS_HPP
+
+
+
+
+
+
+
 
 

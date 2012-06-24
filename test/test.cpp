c4cc457ba2aa0707004925b18a5dcc5da25ea09f
Vadim Stadnik <vadimstdk@gmail.com>
2012-06-24 21:00:43 +1000

    update code for array based B+ trees

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////////
 //
-//          Copyright Vadim Stadnik 2011.
+//          Copyright Vadim Stadnik 2011-2012.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
@@ -26,7 +26,16 @@ int test_main ( int , char * [ ] )
     test_bpt::adapters         ( sz_test , num_dupl ) ;
     test_bpt::adapters_idx     ( sz_test , num_dupl ) ;
     test_bpt::adapters_idx_acc ( sz_test , num_dupl ) ;
+    test_bpt::adapters_array   ( sz_test , num_dupl ) ;
 
     return 0 ;
 }
+
+
+
+
+
+
+
+
 
